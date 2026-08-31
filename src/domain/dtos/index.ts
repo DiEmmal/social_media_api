27b7214f354107a9071635a65c93587488f73c64
DiEmmal <ddelacruz2623@gmail.com>
2026-08-31 15:14:25 -0600

    add password hashing

---
+++ src/domain/dtos/index.ts
@@ -0,0 +1,2 @@
+export * from './auth/register-user.dto.js';
+export * from './auth/login-user.dto.js';
--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -2,14 +2,15 @@ import { Request, Response } from 'express';
 import { CreateUserDto } from '../../domain/dtos/auth/register-user.dto.js';
 import { getUUID } from '../../config/uuid.adapter.js';
 import { LoginUserDto } from '../../domain/dtos/auth/login-user.dto.js';
+import { PasswordServiceImpl } from '../../infrastructure/services/password.service.impl.js';
 
 const users: { id: string, name: string, email: string, password: string }[] = [];
 
 export class AuthController {
-  register = (_req: Request, res: Response) => {
-    const { error, dto } = CreateUserDto.create(_req.body);
+  register = async (req: Request, res: Response) => {
+    const { error, dto } = CreateUserDto.create(req.body);
 
-    if (error) res.status(400).json({ error });
+    if (error) return res.status(400).json({ error });
 
     const { name, email, password } = dto!;
 
@@ -17,29 +18,31 @@ export class AuthController {
       id: getUUID(),
       name: name,
       email: email,
-      password: password
+      password: await PasswordServiceImpl.hash(password),
     };
 
     users.push(newUser);
 
-    res.status(200).json({ message: 'User login successful', user: newUser });
+    return res.status(200).json({ message: `User registered successful, welcome ${newUser.name}!`, user: newUser });
   };
 
-  login = (req: Request, res: Response) => {
+  login = async (req: Request, res: Response) => {
     const { error, dto } = LoginUserDto.create(req.body);
 
-    if (error) res.status(400).json({ error });
+    if (error) return res.status(400).json({ error });
 
     const { email, password } = dto!;
 
     const user = users.find(user => user.email === email);
 
-    if(!user) res.status(404).json({ error: 'User not found' });
+    if (!user) return res.status(404).json({ error: 'User not found' });
 
-    if(user?.password === password) {
-      res.status(200).json({ message: 'User login successful', user });
+    const passwordVerified = await PasswordServiceImpl.compare(password, user?.password!)
+
+    if (passwordVerified) {
+      return res.status(200).json({ message: `User login successful, hi again ${user?.name}!`, user });
     } else {
-      res.status(401).json({ error: 'Invalid password' });
+      return res.status(401).json({ error: 'Invalid password' });
     };
 
   };
